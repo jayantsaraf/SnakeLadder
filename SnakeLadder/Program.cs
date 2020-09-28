5d18839685bf57dfeabfb06774619d8805791855
jayantsaraf <sarafjayant24@gmail.com>
2020-09-28 14:48:30 +0530

    [jayant] Add. A number between 1-6 is generated

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -7,6 +7,14 @@ namespace SnakeLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder game");
+            int pos = 0;
+            Console.WriteLine("Start Position of Player 1 = 0");
+
+            Random random = new Random();
+            ////Generating value on a dice
+            int dice = random.Next(1, 7);
+            Console.WriteLine("The number on dice =" + dice );
+
         }
     }
 }
