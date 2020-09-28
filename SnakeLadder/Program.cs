8277b35d97ea9881246b6981b3bb53f27226fc85
jayantsaraf <sarafjayant24@gmail.com>
2020-09-28 14:39:42 +0530

    [jayant] Add. Start position declared

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -7,6 +7,8 @@ namespace SnakeLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder game");
+            int pos = 0;
+            Console.WriteLine("Start Position of Player 1 = 0");
         }
     }
 }
