142fe2b32640b23e364376c4cec7f5763a89cf25
jayantsaraf <sarafjayant24@gmail.com>
2020-09-28 15:18:10 +0530

    [jayant] Add. added function to repeat the game till player reaches 100

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -7,29 +7,43 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Snake and Ladder game");
             int pos = 0;
             Console.WriteLine("Start Position of Player 1 = 0");
 
-
-            Random random = new Random();
-            ////Generating value on a dice
-            int dice = random.Next(1, 7);
-            Console.WriteLine("The number on dice =" + dice );
-
-            ////Asking for option from user
-            Console.WriteLine("Enter a number - 1. No play, 2.Ladder, 3.Snake");
-            int option = Console.Read();
-            switch(option)
+            while(pos<=100)
             {
-                case 1: pos = dice;
+                
+                Random random = new Random();
+                ////Generating value on a dice
+                int dice = random.Next(1, 7);
+                Console.WriteLine("The number on dice =" + dice);
+
+                ////Asking for option from user
+                Console.WriteLine("Enter a number - 1. No play, 2.Ladder, 3.Snake");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        pos = dice;
                         break;
-                case 2: pos = pos + dice;
+                    case 2:
+                        pos = pos + dice;
                         break;
-                case 3: pos = pos - dice;
+                    case 3:
+                        pos = pos - dice;
                         break;
-                default: break;
+                    default: Console.WriteLine("Enter correct option");
+                             break;
+
+                }
+                if (pos < 0)
+                    pos = 0;
+                Console.WriteLine("The position of player =" + pos);
+
             }
+            
 
 
         }
